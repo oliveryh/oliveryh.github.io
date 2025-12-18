c22167a1f33ef0a5006018270a82007b91e4406b
Oliver Yerbury-Hodgson <oyerburyhodgson@gmail.com>
2025-12-18 17:33:18 +0000

    :wrench: chore: add new theme files (#16)

---
+++ src/consts.ts
@@ -0,0 +1,47 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'oliveryh',
+  description:
+    'Personal blog by oliveryh',
+  href: 'https://oliveryh.com',
+  author: 'oliveryh',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/authors',
+    label: 'authors',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/oliveryh',
+    label: 'GitHub',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
